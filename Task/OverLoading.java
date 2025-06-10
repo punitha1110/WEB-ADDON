d063a00b4691b04066683c5db94d598415ed12a1
punitha1110 <2326ka36@kgcas.com>
2025-06-10 12:40:00 +0530

    Update OverLoading.java

--- Task/OverLoading.java
+++ Task/OverLoading.java
@@ -1,10 +1,10 @@
 class OverLoading{
     public void sum(int a,int b){
-	 System.out.println("sum (a, b)");
+	 System.out.println("Integer of a and b");
         System.out.println("sum((a+b)):"+(a+b));
     }
     public void sum(double a,double b){
-        System.out.println("sum (a, b)");
+        System.out.println("Double of a and b");
         System.out.println("sum((a+b)):"+(a+b));
     }
     public static void main(String[] args){
