b6c5712ccb89feebc72ba41e5b2e36fe6eaf3939
punitha1110 <2326ka36@kgcas.com>
2025-06-10 12:37:58 +0530

    Create OverLoading.java

---
+++ Task/OverLoading.java
@@ -0,0 +1,15 @@
+class OverLoading{
+    public void sum(int a,int b){
+	 System.out.println("sum (a, b)");
+        System.out.println("sum((a+b)):"+(a+b));
+    }
+    public void sum(double a,double b){
+        System.out.println("sum (a, b)");
+        System.out.println("sum((a+b)):"+(a+b));
+    }
+    public static void main(String[] args){
+        OverLoading ref=new OverLoading();
+        ref.sum(2,3);
+        ref.sum(10000,20000);
+    }
+}
