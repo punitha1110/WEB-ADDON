bda1214a691ec05f318906c9f78f262c96ce88cf
punitha1110 <2326ka36@kgcas.com>
2025-06-11 14:12:54 +0530

    Create Finally.java

---
+++ DAY05/Finally.java
@@ -0,0 +1,13 @@
+public class Finally{
+public static void main(String args[]){
+try{
+int[] mynum={1,2,3};
+System.out.println(mynum[10]);
+}catch(Exception e){
+System.out.println("Something went wrong");
+}
+finally{
+System.out.println(The try catch is finished");
+}
+}
+}
