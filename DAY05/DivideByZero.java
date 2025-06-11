e05c644bcba3934cfd4a5052a9d4ff328e6f0b3c
punitha1110 <2326ka36@kgcas.com>
2025-06-11 12:40:40 +0530

    Update DivideByZero.java

--- DAY05/DivideByZero.java
+++ DAY05/DivideByZero.java
@@ -1,13 +1,13 @@
 package prgm;
 import java.util.Scanner;
-class DivideByZero
+class DivideByZero  //error handling
 {
     public static void main(String[] args) {
         Scanner in=new Scanner(System.in);
         try {
             System.out.println("Enter the number:");
             int number=in.nextInt();
-            int total=10/number;
+            int total=10/number; //total=10/5:2
             System.out.println("Total: "+total);
         } catch (Exception e) {
             System.out.println(e);
