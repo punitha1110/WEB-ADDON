94da99b1e665178d734512c95e8cb82f11da8405
punitha1110 <2326ka36@kgcas.com>
2025-06-20 11:58:25 +0530

    Update JDBC.sql

--- DAY12/JDBC.sql
+++ DAY12/JDBC.sql
@@ -1,31 +1,18 @@
 package Mysqlprogram;
-
-
-
 import java.sql.*;
-
-
-
-public class Task1 {
+public class JDBC {
 
 	public static void main(String[] args) throws Exception {
-
-
 
 	String url = "jdbc:mysql://localhost:3306/employee";
 
 	String username = "root";
 
 	String password = "";
-
-
-
+	
 	Connection connect = DriverManager.getConnection(url, username, password);
-
+	
 	System.out.println("Driver Loaded");
 
-
-
 }
-
 }
